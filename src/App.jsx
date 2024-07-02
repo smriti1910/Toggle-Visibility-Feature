9d7a81a2592175595aa274f171368e75d145ee18
smriti1910 <smritibalaji19@gmail.com>
2024-07-02 21:54:31 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <React.Fragment>
       <div className='main-div'>
-        <img src={icon} alt="icon" width={90} height={90}></img>
+        <img src={icon} alt="icon" width={90} height={90}/>
         <h2>Name: {name}</h2>
         <p>Registration No: {regno}</p>
         {/* <p>{student}</p> */}
